011c4ce5fae783077fe172b8d0aaca81f4c83ce3
inesamirzoyan <inesamirzoyan07@gmail.com>
2025-01-19 16:32:39 +0400

    Add files via upload

---
+++ 20.01.2025/app.js
@@ -0,0 +1,29 @@
+// var s = 400;
+// var v = 110;
+// var t = 0;
+// t = s / v;
+// alert(t)
+
+// var s = 1210;
+// var t = 5;
+// var v = 0;
+// v = s / t;
+// alert(v)
+
+var qash = prompt("Greq dzer qashy");
+var banadzev = qash * 0.03;
+document.write("Dzez anhrajesht e motavorapes" + banadzev + "litr jur orakan");
+alert("Dzez anhrajesht e motavorapes" + banadzev + "litr jur orakan")
+
+// var dram = prompt("Mutqagrel gumary");
+// var evro = 412;
+// var rublI = 3.82;
+// var gel = 140;
+// EUR = dram / evro;
+// RUB = dram / rubli;
+// GEL = dram / gel;
+// document.write("Dzer gumary kazmum e <br><br>" + "EUR <br><br>" + "RUB <br><br>" + "GEL <br><br>");
+// alert(EUR);
+// alert(RUB);
+// alert(GEL);
+
