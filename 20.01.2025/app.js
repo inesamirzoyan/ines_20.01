06b957db9fbbfa77ceaa9783bff659c9754a15d9
inesamirzoyan <inesamirzoyan07@gmail.com>
2025-03-24 10:28:02 +0400

    Update app.js

--- 20.01.2025/app.js
+++ 20.01.2025/app.js
@@ -10,20 +10,20 @@
 // v = s / t;
 // alert(v)
 
-var qash = prompt("Greq dzer qashy");
-var banadzev = qash * 0.03;
-document.write("Dzez anhrajesht e motavorapes" + banadzev + "litr jur orakan");
-alert("Dzez anhrajesht e motavorapes" + banadzev + "litr jur orakan")
+// var qash = prompt("Greq dzer qashy");
+// var banadzev = qash * 0.03;
+// document.write("Dzez anhrajesht e motavorapes" + banadzev + "litr jur orakan");
+// alert("Dzez anhrajesht e motavorapes" + banadzev + "litr jur orakan")
 
-// var dram = prompt("Mutqagrel gumary");
-// var evro = 412;
-// var rublI = 3.82;
-// var gel = 140;
-// EUR = dram / evro;
-// RUB = dram / rubli;
-// GEL = dram / gel;
-// document.write("Dzer gumary kazmum e <br><br>" + "EUR <br><br>" + "RUB <br><br>" + "GEL <br><br>");
-// alert(EUR);
-// alert(RUB);
-// alert(GEL);
+var dram = prompt("Mutqagrel gumary");
+var evro = 412;
+var rublI = 3.82;
+var gel = 140;
+EUR = dram / evro;
+RUB = dram / rubli;
+GEL = dram / gel;
+document.write("Dzer gumary kazmum e <br><br>" + "EUR <br><br>" + "RUB <br><br>" + "GEL <br><br>");
+alert(EUR);
+alert(RUB);
+alert(GEL);
 
